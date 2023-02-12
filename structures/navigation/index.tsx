d4b85f8a02661d38f67d4ac8726585f2f83f04c4
Rachouan Rejeb <info@rachouan.com>
2023-02-12 20:00:33 +0000

    Updated logo

--- structures/navigation/index.tsx
+++ structures/navigation/index.tsx
@@ -1,11 +1,15 @@
 import Container from "@/components/container";
+import Logo from "@/components/logo";
 
 export default function Navigation() {
   return (
     <header className="w-full fixed top-0 z-10">
       <Container>
-        <nav className="py-4">
-          <a href="/">Globale</a>
+        <nav className="py-4 flex justify-center w-full">
+          <a href="/">
+            <Logo className="text-teal-500 h-10" />
+            <span className="sr-only">Globale</span>
+          </a>
         </nav>
       </Container>
     </header>
